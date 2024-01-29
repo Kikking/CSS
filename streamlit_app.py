0720645c6fbe805f2cf2cd8007b784cca6c58133
Kikking <63372077+Kikking@users.noreply.github.com>
2024-01-29 15:22:47 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 15:08:32 2024
+
+@author: User
+"""
+
+import streamlit as str
+
+str.title("Test Title")
+str.write("include plot here")
+str.text_input("What plot?:", "bar graph?")
